819bd656935ed65e7ec6af5a92e616f377b1ef12
Gustavo Erick <gustavoerickv@gmail.com>
2024-11-26 21:00:07 -0300

    subindo codigo completo

--- app/src/main/java/com/example/lifecycledemo/MainActivity.kt
+++ app/src/main/java/com/example/lifecycledemo/MainActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lifecycledemo.ui.theme.LifecycleDemoTheme
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Mensagem de log e Toast para onCreate
         Log.d("LifecycleDemo", "onCreate chamado")
         Toast.makeText(this, "onCreate chamado", Toast.LENGTH_SHORT).show()
     }
@@ -24,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        // Mensagem de log e Toast para onStart
         Log.d("LifecycleDemo", "onStart chamado")
         Toast.makeText(this, "onStart chamado", Toast.LENGTH_SHORT).show()
     }
@@ -32,7 +25,6 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        // Mensagem de log e Toast para onResume
         Log.d("LifecycleDemo", "onResume chamado")
         Toast.makeText(this, "onResume chamado", Toast.LENGTH_SHORT).show()
     }
@@ -40,7 +32,6 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
 
-        // Mensagem de log e Toast para onPause
         Log.d("LifecycleDemo", "onPause chamado")
         Toast.makeText(this, "onPause chamado", Toast.LENGTH_SHORT).show()
     }
@@ -48,7 +39,6 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
 
-        // Mensagem de log e Toast para onStop
         Log.d("LifecycleDemo", "onStop chamado")
         Toast.makeText(this, "onStop chamado", Toast.LENGTH_SHORT).show()
     }
@@ -56,26 +46,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
 
-        // Mensagem de log e Toast para onDestroy
         Log.d("LifecycleDemo", "onDestroy chamado")
         Toast.makeText(this, "onDestroy chamado", Toast.LENGTH_SHORT).show()
-    }
-
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LifecycleDemoTheme {
-        Greeting("Android")
     }
 }
